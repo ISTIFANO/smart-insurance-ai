e01cc1f6ae59f4ddcd0694d1b345896f0498fc61
ISTIFANO <elamiriamir7@gmail.com>
2025-09-29 12:43:13 +0200

    SIA-7 feat: charger et vérifier les données afin de m’assurer qu’elles sont exploitables.

--- src/infrastructure/data_loader.py
+++ src/infrastructure/data_loader.py
@@ -1,1 +1,5 @@
-# data_loader
+import pandas as pd
+
+path = r"C:\Users\aamir\Desktop\YC\P\mart-insurance-ai\src\infrastructure\data\assurance-maladie-68d92978e362f464596651.csv"
+data = pd.read_csv(path)
+
