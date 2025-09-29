99b5ad5c17a081872369e3676bd398a19f95fa46
ISTIFANO <elamiriamir7@gmail.com>
2025-09-29 17:13:51 +0200

    Créer des visualisations (histogrammes, boxplots, heatmap corrélations)

--- src/infrastructure/data_loader.py
+++ src/infrastructure/data_loader.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+
 import matplotlib.pyplot as plt
 
 path = r"C:\Users\aamir\Desktop\YC\P\mart-insurance-ai\src\infrastructure\data\assurance-maladie-68d92978e362f464596651.csv"
@@ -31,16 +33,25 @@ print(data.duplicated().sum())
 
 # Analyser la distribution des variables numériques (ex. : histogrammes avec Matplotlib/Seaborn).
 
-cols = data.select_dtypes(include=['int64', 'float64']).columns
+# cols = data.select_dtypes(include=['int64', 'float64']).columns
 
-for col in cols:
-    plt.figure(figsize=(6,4))
-    plt.hist(data[col], bins=20, color='skyblue', edgecolor='black')
-    plt.title(f"distribution de {col}")
-    plt.xlabel(col)
-    plt.ylabel("Fréquence")
-    plt.show()
+# for col in cols:
+#     plt.figure(figsize=(6,4))
+#     plt.hist(data[col], bins=20, color='skyblue', edgecolor='black')
+#     plt.title(f"distribution de {col}")
+#     plt.xlabel(col)
+#     plt.ylabel("Fréquence")
+#     plt.show()
 
+# les relations entre variables à l'aide de matrices de corrélation et de visualisations (ex. : pairplots ou heatmaps).
 
+mat_cor = data.corr(numeric_only=True)
 
+plt.figure(figsize=(9,7))
+sns.heatmap(mat_cor,annot=True, fmt=".2f", cmap="coolwarm", cbar=True, square=True)
+plt.show()
     
+
+sns.pairplot(data,hue='age')
+plt.show()
+
