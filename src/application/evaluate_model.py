2f7542ea7f460e21a87e831cbde7520b18f1b059
ISTIFANO <elamiriamir7@gmail.com>
2025-09-30 23:42:14 +0200

    SIA-11 feat :   les performances des modèles sur l'ensemble de test en utilisant les métriques RMSE (meansquarederror), MAE (meanabsoluteerror), et R² (r2_score)

--- src/application/evaluate_model.py
+++ src/application/evaluate_model.py
@@ -1,1 +1,12 @@
-#evaluate_model
+from sklearn.metrics import mean_squared_error, r2_score,mean_absolute_error
+import numpy as np
+
+def evaluate_model(pipeline, X_test, y_test):
+
+    y_pred = pipeline.predict(X_test)
+    
+    r2 = r2_score(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
+
+    return r2, rmse,mae
