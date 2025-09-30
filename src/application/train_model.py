36fa59ea8f3d5841af98c26b51d07b84757826e0
ISTIFANO <elamiriamir7@gmail.com>
2025-09-30 23:20:58 +0200

    SIA-10 feat : Entraîner les modèles suivants avec des paramètres par défaut : Régression linéaire (LinearRegression), Random Forest (RandomForestRegressor), XGBoost (XGBRegressor), et SVR (SVR)

--- src/application/train_model.py
+++ src/application/train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
@@ -11,12 +11,37 @@ from xgboost import XGBRegressor
 path = r"C:\Users\aamir\Desktop\YC\P\mart-insurance-ai\src\infrastructure\data\assurance-maladie-68d92978e362f464596651.csv"
 data = pd.read_csv(path)
 
-
 X = data.drop("charges", axis=1)
 y = data["charges"]
 
 numeric_features = X.select_dtypes(include=["int64", "float64"]).columns
 categorical_features = X.select_dtypes(include=["object"]).columns
 
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", StandardScaler(), numeric_features),
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features)
+    ]
+)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+models = {
+    "Linear Regression": LinearRegression(),
+    "Random Forest": RandomForestRegressor(random_state=42),
+    "XGBoost": XGBRegressor(random_state=42, verbosity=0),
+    "SVR": SVR()
+}
+
+
+for name, model in models.items():
+    pipeline = Pipeline(steps=[
+        ("preprocessor", preprocessor),
+        ("model", model)
+    ])
+    
+    pipeline.fit(X_train, y_train)
+    
+    y_pred = pipeline.predict(X_test)
+    
+   
