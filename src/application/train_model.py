23da7a90a2b6ea62cae876b882c9a08360d5067e
ISTIFANO <elamiriamir7@gmail.com>
2025-09-30 16:24:57 +0200

    SIA-10 feat :  Entraîner les modèles

--- src/application/train_model.py
+++ src/application/train_model.py
@@ -1,1 +1,22 @@
-#train_model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+
+path = r"C:\Users\aamir\Desktop\YC\P\mart-insurance-ai\src\infrastructure\data\assurance-maladie-68d92978e362f464596651.csv"
+data = pd.read_csv(path)
+
+
+X = data.drop("charges", axis=1)
+y = data["charges"]
+
+numeric_features = X.select_dtypes(include=["int64", "float64"]).columns
+categorical_features = X.select_dtypes(include=["object"]).columns
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
